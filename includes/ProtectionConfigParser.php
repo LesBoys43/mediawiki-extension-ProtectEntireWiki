2d30abc85d2f68dcef980e9c0cc64e1a8ea0c9da
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-21 21:12:36 +0800

    rename for methods

--- includes/ProtectionConfigParser.php
+++ includes/ProtectionConfigParser.php
@@ -3,11 +3,11 @@ class ProtectionConfigParser {
 	public function __construct() {
 		throw new BadMethodCallException("This is a static-only-class");
 	}
-	public static parseCustomTxt($txt) {
+	public static parseFrom($txt) {
 		$txt_lines = explode("\n", $txt);
 		return [trim($txt_lines[0]) == "*"];
 	}
-	public static parse() {
+	public static parseFromConf() {
 		$loc = $wgPEWProtectionConfigFileLoc ?? $IP . "/extensions/ProtectEntireWiki/pconf.txt";
 		if (!file_exists($loc)) {
 			throw new LogicException("Check the LocalSettings, the protection config file not exist");
--- includes/ProtectionConfigWriter.php
+++ includes/ProtectionConfigWriter.php
@@ -3,15 +3,15 @@ class ProtectionConfigWriter {
 	public function __construct() {
 		throw new BadMethodCallException("This is a static-only-class");
 	}
-	public static function writeDataTo($data, $loc) {
+	public static function writeTo($data, $loc) {
 		if (!file_exists($loc)) {
 			throw new BadMethodCallException("Config file must exist");
 		}
 		$cont = implode("\n", $data);
 		return file_put_contents($cont);
 	}
-	public static function writeDataToConf($data) {
+	public static function writeToConf($data) {
 		$loc = $wgPEWProtectionConfigFileLoc ?? $IP . "/extensions/ProtectEntireWiki/pconf.txt";
-		return self::writeDataTo($data, $loc);
+		return self::writeTo($data, $loc);
 	}
 }
