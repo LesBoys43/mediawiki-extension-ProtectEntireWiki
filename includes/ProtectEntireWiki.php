79cacaca79f3f35b74840019d40ccdce5c2ac8ac
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-21 21:15:06 +0800

    stub impl (constructor, destructor, singleton basic impl) for main cls

--- includes/ProtectEntireWiki.php
+++ includes/ProtectEntireWiki.php
@@ -1,4 +1,17 @@
 <?php
 class ProtectEntireWiki {
-
+	public $conf;
+	public $ins = null;
+	private function __construct() {
+		this->conf = ProtectionConfigParsr::parseFromConf();
+	}
+	public function __destruct() {
+		ProtectionConfigWriter::writeToConf(this->$conf);
+	}
+	public static function getInstance() {
+		if ($ins == null) {
+			this->ins = new self();
+		}
+		return this->ins;
+	}
 }
