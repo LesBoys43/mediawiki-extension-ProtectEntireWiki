2c19787e3b1695a9f33215761541f58b17654eb2
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-21 21:15:29 +0800

    typo

--- includes/ProtectEntireWiki.php
+++ includes/ProtectEntireWiki.php
@@ -3,15 +3,15 @@ class ProtectEntireWiki {
 	public $conf;
 	public $ins = null;
 	private function __construct() {
-		this->conf = ProtectionConfigParsr::parseFromConf();
+		$this->conf = ProtectionConfigParsr::parseFromConf();
 	}
 	public function __destruct() {
-		ProtectionConfigWriter::writeToConf(this->$conf);
+		ProtectionConfigWriter::writeToConf($this->$conf);
 	}
 	public static function getInstance() {
 		if ($ins == null) {
-			this->ins = new self();
+			$this->ins = new self();
 		}
-		return this->ins;
+		return $this->ins;
 	}
 }
