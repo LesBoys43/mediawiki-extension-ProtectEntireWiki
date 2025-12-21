8cc8df54a7e7a93e57503ac1aebac67d87f5c49a
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-21 21:07:52 +0800

    add a utility class to write config

---
+++ includes/ProtectionConfigWriter.php
@@ -0,0 +1,13 @@
+<?php
+class ProtectionConfigWriter {
+	public function __construct() {
+		throw new BadMethodCallException("This is a static-only-class");
+	}
+	public static function writeDataTo($data, $loc) {
+		if (!file_exists($loc)) {
+			throw new BadMethodCallException("Config file must exist");
+		}
+		$cont = implode("\n", $data);
+		return file_put_contents($cont);
+	}
+}
