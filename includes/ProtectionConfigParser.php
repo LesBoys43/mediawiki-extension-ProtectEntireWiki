1a833f71fdc01ddddf3a3fb076576364ae08f172
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-21 21:08:11 +0800

    dont hard code config path

--- includes/ProtectionConfigParser.php
+++ includes/ProtectionConfigParser.php
@@ -3,9 +3,6 @@ class ProtectionConfigParser {
 	public function __construct() {
 		throw new BadMethodCallException("This is a static-only-class");
 	}
-	public static function whereIsConf() {
-		return $IP . "/extensions/ProtectEntireWiki/config.txt";
-	}
 	public static parseCustomTxt($txt) {
 		$txt_lines = explode("\n", $txt);
 		return [trim($txt_lines[0]) == "*"];
