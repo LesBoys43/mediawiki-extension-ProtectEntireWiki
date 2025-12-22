1f3a9576a4a2971f4d5d1c7dcb26c9c1b944306d
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-22 14:32:43 +0800

    unitedize cls naming

--- includes/ProtectionConfigReader.php
+++ includes/ProtectionConfigReader.php
@@ -1,17 +1,17 @@
 <?php
-class ProtectionConfigParser {
+class ProtectionConfigReader {
 	public function __construct() {
 		throw new BadMethodCallException("This is a static-only-class");
 	}
-	public static parseFrom($txt) {
+	public static readFrom($txt) {
 		$txt_lines = explode("\n", $txt);
 		return [trim($txt_lines[0]) == "*"];
 	}
-	public static parseFromConf() {
+	public static readFromConf() {
 		$loc = $wgPEWProtectionConfigFileLoc ?? $IP . "/extensions/ProtectEntireWiki/pconf.txt";
 		if (!file_exists($loc)) {
 			throw new LogicException("Check the LocalSettings, the protection config file not exist");
 		}
-		return self::parseCustomTxt(file_get_contents($loc));
+		return self::readFrom(file_get_contents($loc));
 	}
 }
