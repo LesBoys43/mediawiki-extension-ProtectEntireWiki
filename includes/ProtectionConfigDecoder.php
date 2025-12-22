e683c35d56951fc104f3eac3b4885b2aaa2467a2
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-22 21:23:40 +0800

    add decoder support for group-based(&ns-based) ewp

--- includes/ProtectionConfigDecoder.php
+++ includes/ProtectionConfigDecoder.php
@@ -3,4 +3,22 @@ class ProtectionConfigDecoder {
 	public function __construct() {
 		throw new BadMethodCallException("This is a static-only-class");
 	}
+	
+	public static function decode($cont) {
+		$conf = [];
+		$lns = explode("\n", $cont);
+		foreach ($lns as $ln) {
+			$parted = explode("=", trim($ln));
+			switch (count($parted)):
+				case 1:
+					$conf[$parted[0]] = [bin2hex(random_bytes(24))];
+					break;
+				case 2:
+					$conf[$parted[0]] = explode(",", $parted[1]);
+					break;
+				default:
+					throw new LogicException("Bad config text, too many '=' in one line");
+		}
+		return $conf;
+	}
 }
