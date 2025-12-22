71198dbaff3e7a941e84e9e17d5ff719fd1a4f8b
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-22 14:59:08 +0800

    impl encoding logic

--- includes/ProtectionConfigEncoder.php
+++ includes/ProtectionConfigEncoder.php
@@ -2,4 +2,11 @@ class ProtectionConfigEncoder {
 	public function __construct() {
 		throw new BadMethodCallException("This is a static-only-class");
 	}
+	public static function __encoding_map($ln) {
+		if ($ln === true) return "*";
+		return "";
+	}
+	public static function encode($conf) {
+		return array_map("self::__encoding_map", $conf);
+	}
 }
