9a16323a4db6327d41756842025d7bcf02487552
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-22 16:17:12 +0800

    partial impl checks

--- includes/PEWEditingUIHooksRw.php
+++ includes/PEWEditingUIHooksRw.php
@@ -1,7 +1,14 @@
 <?php
+include_once "ProtectEntireWiki.php";
 use MediaWiki\Hook\EditPage__showEditForm_initialHook;
 class PEWEditingUIHooksRw {
 	public funciton onEditPage__showEditForm_initial($editor, $out = null) {
-		
+		$title = $editor->getTitle();
+		$pew = protectEntireWiki::getInstance();
+		if ($pew->canEdit()) {
+			return;
+		}
+		# Cannot edit now, because entire wiki protected
+		# TODO: Render error info
 	}
 }
