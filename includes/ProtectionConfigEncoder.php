6341d9f7a4279f3c5a3465b4f9ffdc18ed72781e
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-23 13:05:49 +0800

    add block all special encode logic

--- includes/ProtectionConfigEncoder.php
+++ includes/ProtectionConfigEncoder.php
@@ -4,6 +4,9 @@ class ProtectionConfigEncoder {
 		throw new BadMethodCallException("This is a static-only-class");
 	}
 	public static function __encoding_map($ln) {
+		if ($ln[1] == []) {
+			return $ln[0];
+		}
 		return "${ln[0]}=${implode(",", $ln[1])}";
 	}
 	public static function encode($conf) {
