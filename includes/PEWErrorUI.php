9e02379dd73a231fca0f0afdad977810a8a16050
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-23 16:15:00 +0800

    refactor error ui to parted text

--- includes/PEWErrorUI.php
+++ includes/PEWErrorUI.php
@@ -4,9 +4,17 @@ class PEWErrorUI {
 		throw new BadMethodCallException("This is a static-only-class");
 	}
 	public static function getProtectedHTML($reqCtx, $pageTitle) {
-		return $reqCtx
-			->msg('pew-wiki-protected-edit-disallowed-msgbox')
-			->params($pageTitle, $reqCtx->getUser()->getName())
-			->plain();
+		$template = "<div class=\"cdx-card\" id=\"pewbox\"><h3>__PEWBOX_TITLE__</h3><hr/><span class=\"cdx-card__text\" style=\"display: inline\"><span style=\"float: left\"><img src=\"https://upload.wikimedia.org/wikipedia/commons/f/f1/Stop_hand_nuvola.svg\" width=48 height=48 alt=\"Stop hand\"/></span><b>$2</b><bdi dir=\"ltr\">__PEWBOX_TEXT_BEFORE_PAGE__</bdi><b>$1</b><bdi dir=\"ltr\">__PEWBOX_TEXT_AFTER_PAGE__</bdi></span></div>";
+		$actual = strtr(
+			$template,
+			[
+				"__PEWBOX_TITLE__" => $reqCtx->msg("protectentirewiki-edit-disallowed-msgbox-title")->plain(),
+				"__PEWBOX_TEXT_BEFORE_PAGE__" => $reqCtx->msg("protectentirewiki-edit-disallowed-msgbox-text-before-page")->plain(),
+				"__PEWBOX_TEXT_AFTER_PAGE__" => $reqCtx->msg("protectentirewiki-edit-disallowed-msgbox-text-after-page")->plain,
+				"$1" => $pageTitle,
+				"$2" => $reqCtx->getUser()->getName()
+			]
+		);
+		return $actual;
 	}
 }
