8f0bfee60c5e78b748e98befd23826b61635b371
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-23 18:29:38 +0800

    use 2nd title when trying save

---
+++ includes/PEWEditingSavingHooksRw.php
@@ -0,0 +1,24 @@
+<?php
+include_once "ProtectEntireWiki.php";
+include_once "PEWErrorUI.php";
+use MediaWiki\Hook\EditPage__attemptSave;
+use MediaWiki\Context\RequestContext;
+use MediaWiki\Logger\LoggerFactory;
+class PEWEditingUIHooksRw implements EditPage__attemptSave{
+	public function onEditPage__attemptSave($editor) {
+		$title = $editor->getTitle();
+		$pew = ProtectEntireWiki::getInstance();
+		$ctx = $editor->getContext();
+		$title = $editor->getArticle()->getTitle();
+		if ($pew->canEdit($title->getNamespaceKey(), $ctx->getUser())) {
+			return;
+		}
+		# Cannot edit, shows msg and aborts
+		$editor->editFormPageTop .= "<script>window.addEventListener(\"load\", function(){mw.loader.load(\"@wikimedia/codex\")})</script>";
+		# See PEWEditingUIHooksRw.php L26
+		$editor->editFormPageTop .= "<style>#pewbox{display: block; min-height: 96px;}</style>";
+		$html = PEWErrorUI::getProtectedHTML($ctx, $title, true);
+		$editor->editFormPageTop .= $html;
+		return false;
+	}
+}
