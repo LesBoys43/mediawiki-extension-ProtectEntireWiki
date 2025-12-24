9429630cb314e9b698c5f89c72cdc359a67db673
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-24 14:19:07 +0800

    impl rb hook without msg gen

--- includes/PEWRollbackingHooks.php
+++ includes/PEWRollbackingHooks.php
@@ -1,12 +1,32 @@
 <?php
 require_once "ProtectEntireWiki.php";
 use MediaWiki\Page\Hook\RollbackCompleteHook;
-use MediaWiki\User\User;
+use MediaWiki\Revision\SlotRecord;
+use MediaWiki\Revision\RevisionSlots;
 class PEWRollbackingHooks implements RollbackCompleteHook {
 	public function onRollbackComplete($wikiPage, $user, $rev, $curr) {
 		$pew = ProtectEntireWiki::getInstance();
 		if ($pew->canRollback($wikiPage->getTitle()->getNamespaceKey(), $user)) {
 			return true;
 		}
+		$tpTitle = User::newFromIdentity($user)->getTalkPage();
+		$ctx = $wikiPage->getContext();
+		$tp = Article::newFromTitle($tpTitle, $ctx);
+		$tpWp = $tp->getWikiPage();
+		$oldRev = $tpWp->getRevisionRecord();
+		$oldRevSlots = $oldRev->getSlots();
+		$oldMain = $oldRevSlots->getSlot(SlotRecord::MAIN);
+		$oldCont = $oldMain->getContent();
+		$oldWt = ContentHandler::getContent($oldCont);
+		# TODO
+		$newWt = $oldWt . "sth";
+		$newCont = new WikitextContent($newWt);
+		$newSlot = SlotRecord::newUnsaved(SlotRecord::MAIN, $newCont);
+		$newRevSlots = new RevisionSlots([$newSlot]);
+		$newRev = new RevisionRecord($tpWp, $newRevSlots);
+		$actor = User::newFromName($wgPEWNotifyUser ?? "ProtectEntireWiki");
+		$actor->addGroup("bureaucrat");
+		$actor->addGroup("bot");
+		$tpWp->doEditUpdates($newRev, $actor);
 	}
 }
