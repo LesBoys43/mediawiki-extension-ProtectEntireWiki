72e7507857aed5619d257486980ba2610063a94b
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-28 11:06:24 +0800

    add a script for check i18n keys

---
+++ maintenance/doI18nKeysCheck.php
@@ -0,0 +1,38 @@
+<?php
+$files = glob("i18n/*.json");
+$failcnt = 0;
+$e255 = false;
+$main = "i18n/zh.json";
+$mainCont = file_get_contents($main);
+$mainData = null;
+if (!json_validate($mainCont)) {
+	# Due the main file invalid, we cannot continue, because check integrity of other keys requires valid main.
+	exit(255);
+}
+$mainData = json_decode($main);
+foreach ($files as $file) {
+	if ($file == $main) {
+		# We should skip main self
+	}
+	$cont = file_get_contents($file);
+	if (!json_validate($cont)) {
+		# Due the json invalid, we can not continue validation, so skip and mark as exit 255
+		$e255 = true;
+		continue;
+	}
+	$data = json_decode($cont);
+	$fail = false;
+	foreach (array_keys($mainData) as $key) {
+		if (!isset($data[$key])) {
+			echo $file . " missed key " . $key . "\n";
+			$fail = true;
+		}
+	}
+	if ($fail) {
+		echo $file . "missed some keys";
+		$failcnt++;
+	} else {
+		echo $file . "has all required keys";
+	}
+}
+exit($e255 ? 255 : $failcnt);
